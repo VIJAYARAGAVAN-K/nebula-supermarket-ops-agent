e045747a7e5e0ed854bea967e5c0d8946f8e798e
VIJAYARAGAVAN-K <vijaycbe05905@gmail.com>
2026-09-03 14:09:19 +0530

    Add tool registry

---
+++ src/tools/registry.py
@@ -0,0 +1,6 @@
+TOOLS = {
+    "add_product": "Add a new product to inventory",
+    "check_stock": "Check current stock",
+    "receive_stock": "Receive new stock",
+    "low_stock": "Find products below reorder level",
+}
